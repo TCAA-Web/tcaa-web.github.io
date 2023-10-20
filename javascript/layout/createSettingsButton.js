bd71f79f9119f58be9cf96992522452a0f4e6c94
kasperfryd <kfry@techcollege.dk>
2023-10-20 14:36:35 +0200

    fix settings

--- javascript/layout/createSettingsButton.js
+++ javascript/layout/createSettingsButton.js
@@ -2,7 +2,7 @@ import { newEl } from "../helpers/newEl.js";
 
 export function createSettingsButton(_isDesktop){
     let settings = newEl({type:'a'})
-    settings.setAttribute('href', '/pages/settings')
+    settings.setAttribute('href', '../../pages/settings')
     settings.classList.add(!_isDesktop ? 'settingsButton' : 'desktopSettingsButton')
     settings.innerHTML = `<img src="../../assets/appIcons/gear-solid.svg"/>`
     return settings
